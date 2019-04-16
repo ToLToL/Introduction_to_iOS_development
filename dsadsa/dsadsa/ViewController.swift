cdb46365501155d7868f0fd75de390dcf6827ff2
Hyokil KIM <treqh@outlook.com>
2019-04-16 09:52:52 +0200

    Add Dicee App structure and remode .zip

---
+++ dsadsa/dsadsa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dsadsa
+//
+//  Created by ToLToL on 15/04/2019.
+//  Copyright © 2019 Hyokil.KIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
